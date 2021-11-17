016889dcaac5946cf7f4c7723dab34792e68c915
scottsak <48599109+scottsak@users.noreply.github.com>
2021-11-17 01:12:27 -0800

    attempting to add type effect

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-logoGlitches=["/images/matrix1.jpg","/images/matrix2.jpg","/images/matrix3.jpg","/images/matrix4.jpg","/images/matrix5.jpg"]
-let i = 0;
-let direction = true; //true = up, false = down
+logoGlitches=["/images/matrix1.jpg","/images/matrix2.jpg","/images/matrix3.jpg","/images/matrix4.jpg","/images/matrix5.jpg"];
+
 
 const app = express();
 
@@ -20,6 +19,21 @@ app.get("/", function(req, res){
     res.render("home",{logoGlitches:logoGlitches});
 
 });
+var i = 0;
+const message="hello, my name is scott."
+
+
+function typeWriter(){
+  if (i < message.length) {
+    document.getElementById("type").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, 50);
+  }
+}
+$(document).ready( function () {
+typeWriter();
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
