35616b6f1d4a6316895343bcb9498aac4d912464
scottsak <48599109+scottsak@users.noreply.github.com>
2022-07-30 10:37:09 -0700

    removed while loop

--- app.js
+++ app.js
@@ -18,21 +18,31 @@ document.getElementById("defaultOpen").click();
 
 
 //carousel
+
+window.addEventListener('load', function () {
+console.log('done')
 let box = document.querySelector('.carousel-inner');
 let height = box.offsetHeight;
-console.log(height)
-while (height === 0){
-  console.log("in loop")
-  document.querySelector(".carousel-inner").style.height = 'auto';
-  box = document.querySelector('.carousel-inner');
-  height = box.offsetHeight;
-  console.log(height)
-}
-console.log("out of loop")
-const desiredHeight = (height+60);
-console.log("desired height dblock: "+ desiredHeight)
-console.log("changed the height")
-document.querySelector(".carousel-inner").style.height = String(desiredHeight) + 'px';
+let desiredHeight = (height+60);
+  document.querySelector(".carousel-inner").style.height = String(desiredHeight) + 'px';
+})
+
+
+// let box = document.querySelector('.carousel-inner');
+// let height = box.offsetHeight;
+// console.log(height)
+// while (height === 0){
+//   console.log("in loop")
+//   document.querySelector(".carousel-inner").style.height = 'auto';
+//   box = document.querySelector('.carousel-inner');
+//   height = box.offsetHeight;
+//   console.log(height)
+// }
+// console.log("out of loop")
+// const desiredHeight = (height+60);
+// console.log("desired height dblock: "+ desiredHeight)
+// console.log("changed the height")
+// document.querySelector(".carousel-inner").style.height = String(desiredHeight) + 'px';
 // document.querySelector("carousel-inner").style.height = String(desiredHeight) + ' px';
 
 
